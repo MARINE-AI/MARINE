4d2f2fff9dd7fde852fbdd53c1303146a874bf8c
aditya singh <adityasingh789m@gmail.com>
2025-02-03 11:49:25 +0530

    init

--- marine-dashboard/next.config.ts
+++ marine-dashboard/next.config.ts
@@ -3,5 +3,24 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
-
+// next.config.js
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/service-worker.js',
+        headers: [
+          {
+            key: 'Service-Worker-Allowed',
+            value: '/'
+          },
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate'
+          }
+        ]
+      }
+    ];
+  }
+};
 export default nextConfig;
